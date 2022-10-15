94e07ed3c71d3e543520dbf8f604d154d0b1eaa2
Łukasz Stępień <lukaszatepien@gmail.com>
2022-10-15 23:41:09 +0200

    Go back to working code

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 const tasks = document.getElementById("tasks");
+const addButton = document.getElementById("button");
 
 function deleteTask(event) {
     event.target.remove();
 }
 
-tasks.addEventListener("click", deleteTask, false);
+tasks.addEventListener("click", deleteTask);
