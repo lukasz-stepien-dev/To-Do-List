e67a422c972705cdd15653b7c50920e3367cbbf6
Łukasz Stępień <lukaszatepien@gmail.com>
2022-10-16 00:27:54 +0200

    Do add task and change flex-direction

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,28 @@
 const tasks = document.getElementById("tasks");
-const addButton = document.getElementById("button");
+const addTaskWindow = document.getElementById("addTaskWindow");
+const inputTaskName = document.getElementById("taskName")
+addTaskWindow.style.display = "none";
 
-function deleteTask(event) {
+tasks.addEventListener("click", event => {
     event.target.remove();
-}
+});
 
-tasks.addEventListener("click", deleteTask);
+document.getElementById("button").addEventListener("click", () => {
+    if (addTaskWindow.style.display == "none") {
+        addTaskWindow.style.display = "flex";
+    } else {
+        addTaskWindow.style.display = "none";
+    }
+    inputTaskName.value = "";
+});
+
+document.getElementById("submitButton").addEventListener("click", () => {
+    let taskName = document.createElement("li");
+    taskName.textContent = inputTaskName.value;
+    tasks.append(taskName);
+    addTaskWindow.style.display = "none";
+});
+
+document.getElementById("cancelButton").addEventListener("click", () => {
+    addTaskWindow.style.display = "none";
+})
